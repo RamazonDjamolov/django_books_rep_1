2cc53409e7481af60c93cd2f5f5468eccdc92f7e
djamolovramazon <djamolovramazon90@gmail.com>
2025-02-16 12:06:55 +0500

    v1

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path
 
+
+def index(request):
+    return render(request, 'base.html')
+
+
 urlpatterns = [
+
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
 ]
